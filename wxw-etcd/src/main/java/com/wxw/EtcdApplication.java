c03261dc729e0de70b939882ddb5b8079e583cda
weixiaowei <weixiaowei@baijia.com>
2021-07-01 12:36:43 +0800

    etcd

---
+++ wxw-etcd/src/main/java/com/wxw/EtcdApplication.java
@@ -0,0 +1,16 @@
+package com.wxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weixiaowei
+ * @desc:
+ * @date: 2021/7/1
+ */
+@SpringBootApplication
+public class EtcdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EtcdApplication.class, args);
+    }
+}
